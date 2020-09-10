7f021ae7e2886721fd295fae9235066588a454b6
yamada28go@gmail.com <yamada28go@gmail.com>
2020-09-10 22:26:02 +0900

    ・S3データを間違えて上書きしないようにブロック処理を追加

--- cron-backup/shell/syncToS3.sh
+++ cron-backup/shell/syncToS3.sh
@@ -17,8 +17,43 @@ if [ -e $AWS_CONFIG ]; then
 
   source /root/.aws/S3Config.sh
 
-  #S3の設定が確認できた場合
-  flock -n /tmp/s3sync.lock /usr/local/bin/aws s3 sync $SAVEPATH_BASE s3://${S3_TARGET_BUCKET_NAME}/${S3_TARGET_DIRECTORY_NAME} --delete
+  # 対象となるフォルダのファイル数を比較。
+  # 10%以上違っていたら、エラーなのでバックアップは実行しない
+  # 新規にインスタンスを立ち上げて、既存のバックアップはフォルダに対して、
+  # 間違って上書きをしないように対応する。
+
+  S3Files=`/usr/local/bin/aws s3 ls s3://${S3_TARGET_BUCKET_NAME}/${S3_TARGET_DIRECTORY_NAME} --recursive --human | wc -l`
+  LocalFiles=`find $SAVEPATH_BASE -type f | wc -l`
+
+  echo "S3Files : " ${S3Files}
+  echo "LocalFiles : " ${LocalFiles}
+
+  TCAL=(`echo "scale=5; (${S3Files} / ${LocalFiles}) * 100" | bc`)
+  TCAL_R=(`printf '%.f \n' ${TCAL}`)
+  echo "diff ${TCAL_R}"
+
+  # ファイル数の差が5%以内なら指定ディレクトリにコピーする
+  if [ ${TCAL_R} -lt 5 ]; then
+
+    #S3の設定が確認できた場合
+    echo "start S3Sync default path"
+    echo `date +%Y%m%d_%H-%M-%S` >> ${SAVEPATH_BASE}/S3SyncLog.txt
+    flock -n /tmp/s3sync.lock /usr/local/bin/aws s3 sync $SAVEPATH_BASE s3://${S3_TARGET_BUCKET_NAME}/${S3_TARGET_DIRECTORY_NAME} --delete
+  
+  else
+
+    # 5%以上ずれている場合、安全領域にコピーする
+    echo "start S3Sync safe path"
+    echo `date +%Y%m%d_%H-%M-%S` >> ${SAVEPATH_BASE}/S3SyncLog.txt
+    flock -n /tmp/s3sync.lock /usr/local/bin/aws s3 sync $SAVEPATH_BASE s3://${S3_TARGET_BUCKET_NAME}/${S3_TARGET_DIRECTORY_NAME}-safe --delete
+
+  fi
+
+  echo "end new S3 backup!"
+
+else
+
+  echo "S3 config not found!"
 
 fi
 
