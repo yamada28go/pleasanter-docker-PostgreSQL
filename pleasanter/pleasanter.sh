53280fdbea85fb1ce9b762db142c146edb13a373
yamada28go <yamada28go@gmail.com>
2021-12-24 00:32:12 +0900

    設定パラメータ関係の処理を修正

--- pleasanter/pleasanter.sh
+++ pleasanter/pleasanter.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 export LANG=ja_JP.UTF-8
+
+# パラメータファイルの調整を行う
+# 既存のファイルを書き出しておく
+rsync -rv --delete /home/Pleasanter.NetCore/Implem.Pleasanter.NetCore/App_Data/Parameters_back/* /var/opt/parameters/raw
+# 変更分だけ取り込む
+rsync -rv /var/opt/parameters/customize/* /home/Pleasanter.NetCore/Implem.Pleasanter.NetCore/App_Data/Parameters
 cd `dirname $0`
 cd ../publish/Implem.Pleasanter/
 dotnet Implem.Pleasanter.NetCore.dll --urls=http://0.0.0.0:80 --pathbase=/pleasanter
