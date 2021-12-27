2940915d417f34a5577a8e7fed22f04cbc4ebf58
yamada28go <yamada28go@gmail.com>
2021-12-27 23:14:57 +0900

    設定ファイルをコピーするように修正

--- pleasanter/pleasanter.sh
+++ pleasanter/pleasanter.sh
@@ -5,7 +5,7 @@ export LANG=ja_JP.UTF-8
 # 既存のファイルを書き出しておく
 rsync -rv --delete /home/Pleasanter.NetCore/Implem.Pleasanter.NetCore/App_Data/Parameters_back/* /var/opt/parameters/raw
 # 変更分だけ取り込む
-rsync -rv /var/opt/parameters/customize/* /home/Pleasanter.NetCore/Implem.Pleasanter.NetCore/App_Data/Parameters
+rsync -rv /var/opt/parameters/customize/* /home/Pleasanter.NetCore/publish/Implem.Pleasanter/App_Data/Parameters
 cd `dirname $0`
 cd ../publish/Implem.Pleasanter/
 dotnet Implem.Pleasanter.NetCore.dll --urls=http://0.0.0.0:80 --pathbase=/pleasanter
