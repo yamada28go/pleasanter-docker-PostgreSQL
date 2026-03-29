f303aa551755e3707ae68bb4c133c77741aeede4
yamada28go <yamada28go@gmail.com>
2026-03-29 14:04:11 +0900

    dbパスを修正

--- cron-backup/shell/pg_rman_env.sh
+++ cron-backup/shell/pg_rman_env.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-export PATH=$PATH:/usr/lib/postgresql/15/bin/
+
+PG_MAJOR="${POSTGRES_VERSION:-18}"
+export PATH="$PATH:/usr/lib/postgresql/${PG_MAJOR}/bin/"
 
 # アーカイブログの保持日数を指定
 export KEEP_ARCLOG_DAYS=1
@@ -9,10 +11,8 @@ export KEEP_DATA_DAYS=1
 # バックアップ先ディレクトリ
 export SAVEPATH_BASE='/var/db_backup/PITR'
 export BACKUP_PATH=$SAVEPATH_BASE
-export PGDATA=/var/lib/postgresql/data
+export PGDATA="${POSTGRES_VOLUMES_TARGET:-/var/lib/postgresql/data}"
 export ARCLOG_PATH=/var/lib/postgresql/arclog
 
 # ユーザー設定
 export HOST_CONFIG='--host=postgres-db --username postgres'
-
-
